3480266ec94a1812ccd9d3e57183e59f8c089d1d
Braintree <code@getbraintree.com>
2013-09-25 01:41:28 +0000

    namespace project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject eventstream "0.1.0-SNAPSHOT"
+(defproject pitluga/eventstream "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
